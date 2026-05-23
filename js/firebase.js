c93cca48405b60a635eb8083269cf1a8e49b461d
mariaeduardatavares997 <mariaeduardatavares997@gmail.com>
2026-05-23 17:37:18 -0300

    Alteração no firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,4 +1,14 @@
 //Configuração REAL do Firebase:
+// IMPORTS FIREBASE
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth }
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getDatabase }
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { getStorage }
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBdL9LRgnuHn2jvzf8jL9wnoEaahs50j2w",
   authDomain: "solaria-smt.firebaseapp.com",
