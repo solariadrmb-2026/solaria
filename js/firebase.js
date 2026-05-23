188a32e9eba8d9bc22a84bcec4b056c4babd8f35
gigi982 <mariarodriguesgomesdasilva4@gmail.com>
2026-05-23 19:52:03 -0300

    adição de import firebase

--- js/firebase.js
+++ js/firebase.js
@@ -1,3 +1,13 @@
+// IMPORTS FIREBASE
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth }
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getDatabase }
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { getStorage }
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+
 //Configuração REAL do Firebase:
 const firebaseConfig = {
   apiKey: "AIzaSyBdL9LRgnuHn2jvzf8jL9wnoEaahs50j2w",
